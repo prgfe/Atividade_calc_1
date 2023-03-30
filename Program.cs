fe71b44accfa3bc6a9c5c111df03afb6b4beddad
Felipe <febatistabaptista@gmail.com>
2023-03-30 11:05:12 -0300

    atividade calculadora 1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Faça um programa que receba a idade de uma pessoa e calcule a idade em meses, dias, horas e minutos.
+// Imprima o resultado no console.
+
+Console.WriteLine($"Seja bem-vindo ao nosso sistema! Digite a sua idade em anos: ");
+int idade = int.Parse(Console.ReadLine());
+
+int converteMes = (idade * 12);
+
+int converteDias = (converteMes * 30);
+
+int converteHoras = (converteDias * 24);
+
+Console.WriteLine($"Sua idade é de: {idade} anos, {converteMes} meses, {converteDias} dias e {converteHoras} horas");
